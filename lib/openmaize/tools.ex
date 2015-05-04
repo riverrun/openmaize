e3eab887aafa463237a9b3fd918eb18a9051d8fd
David Whitlock <alovedalongthe@gmail.com>
2015-05-04 16:46:02 +0700

    Further changes to redirect

--- lib/openmaize/tools.ex
+++ lib/openmaize/tools.ex
@@ -6,7 +6,7 @@ defmodule Openmaize.Tools do
   alias Openmaize.Config
 
   def redirect_to_login(conn) do
-    uri = "https://" <> conn.host <> Config.login_page
+    uri = "http://" <> conn.host <> Config.login_page
     conn
     |> put_resp_header("location", uri)
     |> put_status(301)
