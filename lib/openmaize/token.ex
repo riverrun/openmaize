7928aca10cc27f99ee3bc14efa7c0542e4b6e6bf
David Whitlock <alovedalongthe@gmail.com>
2015-05-17 19:12:44 +0700

    Updated joken token function

--- lib/openmaize/token.ex
+++ lib/openmaize/token.ex
@@ -14,15 +14,15 @@ defmodule Openmaize.Token do
   @moduledoc """
   """
 
-  @secret_key Config.secret_key
+  @secret_key Openmaize.Config.secret_key
   @json_module Openmaize.JSON
   @algorithm :HS512
 
-  def encode(claims) do
-    Joken.encode(@secret_key, @json_module, payload, @algorithm, claims \\ %{})
+  def encode(payload, claims \\ %{}) do
+    Joken.Token.encode(@secret_key, @json_module, payload, @algorithm, claims)
   end
 
-  def decode(token) do
-    Joken.decode(@secret_key, @json_module, token, @algorithm, claims \\ %{})
+  def decode(token, claims \\ %{}) do
+    Joken.Token.decode(@secret_key, @json_module, token, @algorithm, claims)
   end
 end
