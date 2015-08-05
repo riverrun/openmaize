072ddc1bab054dc31977db526625c479708fb5fc
David Whitlock <alovedalongthe@gmail.com>
2015-08-05 18:30:23 +0700

    Added Authorize Base tests

--- test/authorize_test.exs
+++ test/authorize_test.exs
@@ -3,6 +3,7 @@ defmodule Openmaize.AuthorizeTest do
   use Plug.Test
 
   alias Openmaize.Authorize
+  alias Openmaize.Authorize.Base
 
   @admin %{id: 2, name: "Big Boss", role: "admin"}
   @user %{id: 1, name: "Raymond Luxury Yacht", role: "user"}
@@ -30,6 +31,41 @@ defmodule Openmaize.AuthorizeTest do
             |> Authorize.call([])
             |> send_resp(200, "")
     assert conn.status == 200
+  end
+
+  test "Base authorized? ok conn" do
+    conn = Base.authorized?({:ok, :nomatch}, conn(:get, "/"), []) |> send_resp(200, "")
+    assert conn.status == 200
+    conn = Base.authorized?({:ok, "/users", "/users"}, conn(:get, "/"), []) |> send_resp(200, "")
+    assert conn.status == 200
+  end
+
+  test "Base authorized? send_error 401" do
+    conn = Base.authorized?({:error, "You have 10 minutes to spend it"},
+    conn(:get, "/admin"), {false, false})
+    assert conn.status == 401
+  end
+
+  test "Base authorized? send_error 403" do
+    conn = Base.authorized?({:error, "user", "You have 10 minutes to spend it"},
+    conn(:get, "/admin"), {false, false})
+    assert conn.status == 403
+  end
+
+  test "Base authorized? handle_error with no role" do
+    conn = Base.authorized?({:error, "You have 10 minutes to spend it"},
+    conn(:get, "/admin"), {true, false})
+    assert List.keyfind(conn.resp_headers, "location", 0) ==
+           {"location", "http://www.example.com/admin/login"}
+    assert conn.status == 302
+  end
+
+  test "Base authorized? handle_error with role" do
+    conn = Base.authorized?({:error, "user", "You have 10 minutes to spend it"},
+    conn(:get, "/admin"), {true, false})
+    assert List.keyfind(conn.resp_headers, "location", 0) ==
+           {"location", "http://www.example.com/users"}
+    assert conn.status == 302
   end
 
 end
