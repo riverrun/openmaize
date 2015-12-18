8a1eafcf972e06975d9e5bf9b7112196a79a89d2
David Whitlock <alovedalongthe@gmail.com>
2015-12-18 14:21:09 +0700

    Updated Signup module

--- lib/openmaize/signup.ex
+++ lib/openmaize/signup.ex
@@ -6,23 +6,22 @@ defmodule Openmaize.Signup do
   import NotQwerty123.PasswordStrength
   alias Openmaize.Config
 
-  def login_user(changeset, params) do
+  def create_user(changeset, params) do
     changeset
     |> cast(params, ~w(password), [])
     |> validate_length(:password, min: 8, max: 80)
     |> put_pass_hash()
   end
 
-  def put_pass_hash(changeset) do
+  defp put_pass_hash(changeset) do
     case changeset do
       %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
-        #put_change(changeset, :password_hash, Config.get_crypto_mod.hashpwsalt(password))
-        check_pword_put_change(changeset, password)
+        strong_pword_put_change(changeset, password)
       _ -> changeset
     end
   end
 
-  defp check_pword_put_change(changeset, password, opts \\ []) do
+  defp strong_pword_put_change(changeset, password, opts \\ []) do
     case strong_password?(password, opts) do
       true -> put_change(changeset, :password_hash, Config.get_crypto_mod.hashpwsalt(password))
       message -> add_error(changeset, :password, message)
