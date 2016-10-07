1d5fc960b89a0a21f1af6b37022ff8f685baf818
David Whitlock <alovedalongthe@gmail.com>
2016-10-08 06:32:33 +0700

    Updated documentation for the mailer module in the templates

--- priv/templates/phoenixauth/mailer.ex
+++ priv/templates/phoenixauth/mailer.ex
@@ -10,6 +10,10 @@ defmodule <%= base %>.Mailer do
 
   @doc """
   An email with a confirmation link in it.
+
+  This function is called by the `create` function in the user_controller.
+  It should send an email with the confirmation link to the user. The
+  return value is not used.
   """
   def ask_confirm(_email, link) do
     confirm_url = "http://www.example.com/sessions/confirm_email?#{link}"
@@ -18,6 +22,10 @@ defmodule <%= base %>.Mailer do
 
   @doc """
   An email with a link to reset the password.
+
+  This function is called by the `create` function in the password_reset_controller.
+  It should send an email with the link to reset the password to the user. The
+  return value is not used.
   """
   def ask_reset(_email, link) do
     confirm_url = "http://www.example.com/password_resets/edit?#{link}"
@@ -26,6 +34,11 @@ defmodule <%= base %>.Mailer do
 
   @doc """
   An email acknowledging that the account has been successfully confirmed.
+
+  This function is called by the Openmaize.ConfirmEmail and the
+  Openmaize.ResetPassword plugs. It should send an email stating that the
+  account has been confirmed, or that the password has been reset, to the user.
+  The return value is not used.
   """
   def receipt_confirm(email) do
     email
