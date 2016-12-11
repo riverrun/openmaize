08bbd359be5f67d0cdb30e9e3834f5f1e6032e18
David Whitlock <alovedalongthe@gmail.com>
2016-12-11 22:03:02 +0800

    Added integration test for mix phoenixauth generator

---
+++ integration_test/openmaize_phoenixauth_test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -x
+
+LOG="../openmaize_phoenixauth_test.log"
+
+function new_project {
+    cd alibaba || exit $?
+    sed -i 's/username: "postgres"/username: "dev"/g' config/dev.exs config/test.exs
+    sed -i 's/password: "postgres"/password: System.get_env("POSTGRES_PASS")/g' config/dev.exs config/test.exs
+    sed -i 's/:postgrex]/:postgrex, :openmaize]/g' mix.exs
+    sed -i 's/{:postgrex, ">= 0.0.0"},/{:postgrex, ">= 0.0.0"},\n     {:openmaize, "~> 2.4"},/g' mix.exs
+    #sed -i 's/{:postgrex, ">= 0.0.0"},/{:postgrex, ">= 0.0.0"},\n     {:openmaize, git: "https://gihub.com/riverrun/openmaize"},/g' mix.exs
+}
+
+function enter_cave {
+    mix deps.get
+    echo -e "\nDATE: $(date) OPTIONS: $1\n" >> $LOG
+    echo y | mix openmaize.gen.phoenixauth $1
+}
+
+function run_tests {
+    mix test >> $LOG
+    MIX_ENV=test mix ecto.drop
+}
+
+function clean {
+    cd ..
+    rm -rf alibaba
+}
+
+function openmaize_project {
+    new_project
+    enter_cave $1
+    run_tests
+    clean
+}
+
+cd $(dirname "$0")/../tmp
+echo y | mix phoenix.new alibaba
+openmaize_project
+echo y | mix phoenix.new alibaba
+openmaize_project --confirm
+echo y | mix phoenix.new alibaba --no-html --no-brunch
+openmaize_project --api
