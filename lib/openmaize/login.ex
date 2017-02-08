57f31f21720590ec4ef0a41cbd5752ff1641af7c
David Whitlock <alovedalongthe@gmail.com>
2017-02-08 12:20:34 +0800

    Removed Login function option and kept unique_id option

--- lib/openmaize/login.ex
+++ lib/openmaize/login.ex
@@ -1,29 +1,62 @@
-vars = [{Openmaize.Login, "username", :username},
-        {Openmaize.EmailLogin, "email", :email}]
+defmodule Openmaize.Login do
+  @moduledoc """
+  """
 
-for {mod, user_params, user_id} <- vars do
-  defmodule mod do
-    @moduledoc """
-    Module to handle login using #{user_params} as user id.
+  @behaviour Plug
 
-    There are two options:
+  import Plug.Conn
+  alias Openmaize.{Config, Logger}
 
-      * repo - the name of the repo
-        * the default is MyApp.Repo - using the name of the project
-      * user_model - the name of the user model
-        * the default is MyApp.User - using the name of the project
+  def init(opts) do
+    uniq = Keyword.get(opts, :unique_id, :username)
+    {uniq, to_string(uniq),
+    Keyword.get(opts, :repo, Openmaize.Utils.default_repo),
+    Keyword.get(opts, :user_model, Openmaize.Utils.default_user_model)}
+  end
 
-    See the documentation for Openmaize.Login.Base for information about
-    creating a custom Plug for logins.
-    """
+  def call(%Plug.Conn{params: %{"session" => params}} = conn,
+    {uniq, user_params, repo, user_model}) when is_atom(uniq) do
+    %{^user_params => user_id, "password" => password} = params
+    check_user_pass conn, {uniq, user_id, password}, {repo, user_model}
+  end
+  def call(%Plug.Conn{params: %{"session" => params}} = conn,
+    {uniq, _, repo, user_model}) do
+    check_user_pass conn, uniq.(params), {repo, user_model}
+  end
 
-    use Openmaize.Login.Base
+  @doc """
+  Check the user's password.
+  """
+  def check_user_pass(conn, {uniq, user_id, password}, {repo, user_model}) do
+    repo.get_by(user_model, [{uniq, user_id}])
+    |> check_pass(password, Config.hash_name)
+    |> handle_auth(conn, user_id)
+  end
+  def check_user_pass(_, _, _), do: raise ArgumentError, "invalid params or options"
 
-    unless user_params == "username" do
-      def unpack_params(%{unquote(user_params) => user, "password" => password}) do
-        {unquote(user_id), user, password}
-      end
-      def unpack_params(_), do: nil
-    end
+  defp check_pass(nil, _, _) do
+    Config.crypto_mod.dummy_checkpw
+    {:error, "invalid user-identifier"}
+  end
+  defp check_pass(%{confirmed_at: nil}, _, _), do: {:error, "account unconfirmed"}
+  defp check_pass(user, password, hash_name) do
+    %{^hash_name => hash} = user
+    Config.crypto_mod.checkpw(password, hash) and
+    {:ok, user} || {:error, "invalid password"}
+  end
+
+  defp handle_auth({:ok, %{id: id, otp_required: true}}, conn, _) do
+    put_private(conn, :openmaize_otpdata, id)
+  end
+  defp handle_auth({:ok, user}, conn, _) do
+    put_private(conn, :openmaize_user, user)
+  end
+  defp handle_auth({:error, "acc" <> _ = message}, conn, user_id) do
+    Logger.warn conn, user_id, message
+    put_private(conn, :openmaize_error, "You have to confirm your account")
+  end
+  defp handle_auth({:error, message}, conn, user_id) do
+    Logger.warn conn, user_id, message
+    put_private(conn, :openmaize_error, "Invalid credentials")
   end
 end
--- test/login_test.exs
+++ test/login_test.exs
@@ -4,33 +4,29 @@ defmodule Openmaize.LoginTest do
 
   alias Openmaize.{DummyCrypto, TestRepo, TestUser}
 
-  @opts {TestRepo, TestUser}
-
-  def login_name(name, password, user_id \\ "username") do
+  def login(name, password, uniq \\ :username, user_params \\ "username") do
     conn(:post, "/login",
-         %{"session" => %{user_id => name, "password" => password}})
-    |> Openmaize.Login.call(@opts)
+         %{"session" => %{user_params => name, "password" => password}})
+    |> Openmaize.Login.call({uniq, user_params, TestRepo, TestUser})
   end
 
-  def login_email(name, password) do
-    conn(:post, "/login",
-         %{"session" => %{"email" => name, "password" => password}})
-    |> Openmaize.EmailLogin.call(@opts)
+  def phone_name(%{"phone" => phone, "password" => password}) do
+    {Regex.match?(~r/^[0-9]+$/, phone) and :phone || :username, phone, password}
   end
 
   test "init function" do
-    assert Openmaize.Login.init([]) == {Openmaize.Repo, Openmaize.User}
+    assert Openmaize.Login.init([]) == {:username, "username", Openmaize.Repo, Openmaize.User}
   end
 
   test "login succeeds with username" do
-    conn = login_name("ray", "h4rd2gU3$$")
+    conn = login("ray", "h4rd2gU3$$")
     %{id: id, role: role} = conn.private[:openmaize_user]
     assert id == 4
     assert role == "user"
   end
 
   test "login succeeds with email" do
-    conn = login_email("ray@mail.com", "h4rd2gU3$$")
+    conn = login("ray@mail.com", "h4rd2gU3$$", :email, "email")
     %{id: id, role: role} = conn.private[:openmaize_user]
     assert id == 4
     assert role == "user"
@@ -38,36 +34,30 @@ defmodule Openmaize.LoginTest do
 
   test "login fails when crypto mod changes" do
     Application.put_env(:openmaize, :crypto_mod, DummyCrypto)
-    conn = login_name("ray", "h4rd2gU3$$")
+    conn = login("ray", "h4rd2gU3$$")
     assert conn.private[:openmaize_error]
   after
     Application.delete_env(:openmaize, :crypto_mod)
   end
 
   test "login fails for incorrect password" do
-    conn = login_name("ray", "oohwhatwasitagain")
+    conn = login("ray", "oohwhatwasitagain")
     assert conn.private[:openmaize_error] =~ "Invalid credentials"
   end
 
   test "login fails when account is not yet confirmed" do
-    conn = login_name("fred", "mangoes&g0oseberries")
+    conn = login("fred", "mangoes&g0oseberries")
     assert conn.private[:openmaize_error] =~ "have to confirm your account"
   end
 
   test "login fails for invalid username" do
-    conn = login_name("dick", "h4rd2gU3$$")
+    conn = login("dick", "h4rd2gU3$$")
     assert conn.private[:openmaize_error] =~ "Invalid credentials"
   end
 
   test "login fails for invalid email" do
-    conn = login_email("dick@mail.com", "h4rd2gU3$$")
+    conn = login("dick@mail.com", "h4rd2gU3$$", :email, "email")
     assert conn.private[:openmaize_error] =~ "Invalid credentials"
-  end
-
-  test "error raised for incorrect params" do
-    assert_raise ArgumentError, "invalid params or options", fn ->
-      login_name("081555555", "h4rd2gU3$$", "phone")
-    end
   end
 
 end
